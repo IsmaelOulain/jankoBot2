a0dc41496c83af3f49d422fd60c7405002b73e13
TeaCup2 <ismael2000oulain@gmail.com>
2020-07-26 15:15:30 +0200

    s

--- index.js
+++ index.js
@@ -102,20 +102,20 @@ client.on('message', async message => {
        } else {
          message.reply('big fail fra');
        }
-    }
+    }    
     break;
     case '/whds':{
-      if(message.member.voice.channel){
-        const attachment = new MessageAttachment('https://imgur.com/UJnyJHy.png');
-        const connection = await message.member.voice.channel.join();
-        
-        message.channel.send(attachment);
-        const dispatcher = connection.play('./record/whods.mp3');
-
-
-      }else {
-        message.reply('big fail fra');
-      }
+      if (message.member.voice.channel) {
+        // const ayy = client.emojis.find(emoji => emoji.name === "cha");
+         //message.reply(`${ayy} LMAO`);
+        const attachment = new MessageAttachment('https://imgur.com/UJnyJHy.png' );
+         const connection = await message.member.voice.channel.join();
+         
+         message.channel.send(attachment);
+         const dispatcher = connection.play('./record/whods.mp3');
+       } else {
+         message.reply('big fail fra');
+       }
     }
     break;
   
