50bca5ad88beb220b1d5112e48e67706b7ba4243
TeaCup2 <ismael2000oulain@gmail.com>
2020-07-28 01:29:39 +0200

    sdf

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ client.on('message', async message => {
   // we ignore it
   if (!message.guild) return;
     switch(message.content){
-      case '/help':{
+      /*case '/help':{
         message.reply('cojone che cazzo non capisci');
         message.channel.send('/chandu per sentire il famigerato chandu \n'+'/cesco l inglese di cesco\n'+'/ok AHH OKKKKEEEYYYY\n'+'/eva sisqo che dice per l ennesima volta di vedere evangelion\n'+'/balzo beh.. il king di como lake\n'+'/fnatic la collaborazione che interessa a sisqo');
         message.channel.send('/cesco l inglese di cesco');
@@ -19,10 +19,8 @@ client.on('message', async message => {
         message.channel.send('/eva sisqo che dice per l ennesima volta di vedere evangelion ');
         message.channel.send('/balzo beh.. il king di como lake');
         message.channel.send('/fnatic la collaborazione che interessa a sisqo');
-  
-  
-      }
-      break;
+  }
+      break;*/
       case '/cesco':{
         if (message.member.voice.channel) {
           const connection = await message.member.voice.channel.join();
@@ -215,6 +213,20 @@ client.on('message', async message => {
            
            //message.channel.send(attachment);
            const dispatcher = connection.play('./record/drift.ogg');
+         } else {
+           message.reply('big fail fra');
+         }
+      }
+      break;
+      case '/shiton':{
+        if (message.member.voice.channel) {
+          // const ayy = client.emojis.find(emoji => emoji.name === "cha");
+           //message.reply(`${ayy} LMAO`);
+           //const attachment = new MessageAttachment('https://imgur.com/wJK5ggK.png');
+           const connection = await message.member.voice.channel.join();
+           
+           //message.channel.send(attachment);
+           const dispatcher = connection.play('./record/shiton.mp3');
          } else {
            message.reply('big fail fra');
          }
