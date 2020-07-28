483d0e53cdd239e74913f11d0386bfb2f5945088
TeaCup2 <ismael2000oulain@gmail.com>
2020-07-28 15:43:36 +0200

    broken

--- index.js
+++ index.js
@@ -256,6 +256,21 @@ client.on('message', async message => {
            message.reply('big fail fra');
          }
       }
+      break;
+      case '/broken':{
+        if (message.member.voice.channel) {
+          // const ayy = client.emojis.find(emoji => emoji.name === "cha");
+           //message.reply(`${ayy} LMAO`);
+           //const attachment = new MessageAttachment('https://imgur.com/wJK5ggK.png');
+           const connection = await message.member.voice.channel.join();
+           
+           //message.channel.send(attachment);
+           //dispatcher.setVolume(0.8);
+           const dispatcher = connection.play('./record/broken.mp3');
+         } else {
+           message.reply('big fail fra');
+         }
+      }
     }
   
   
