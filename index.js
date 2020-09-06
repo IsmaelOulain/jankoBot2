9f146321387711ed937d249e850a0e90c5a1da21
TeaCup2 <47120050+TeaCup2@users.noreply.github.com>
2020-09-06 16:30:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -346,7 +346,7 @@ client.on('message', async message => {
         break;
       case '/ciospo':{
         if (message.member.voice.channel) {
-          const attachment = new MessageAttachment('https://imgur.com/a/3BnUmdh.png');
+          const attachment = new MessageAttachment('https://imgur.com/XF2lsG9');
           const connection = await message.member.voice.channel.join();
           message.channel.send(attachment);
           const dispatcher = connection.play('./record/ciospo.opus');
