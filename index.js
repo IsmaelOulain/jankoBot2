d1909519efaefadb7b0ada61cfbc9ba98394218a
TeaCup2 <47120050+TeaCup2@users.noreply.github.com>
2020-09-24 22:21:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -420,6 +420,17 @@ client.on('message', async message => {
           message.reply('a coglione entra in vocale per farlo');
         }
       }
+          break;
+      case '/cares':{
+        if (message.member.voice.channel) {
+          //const attachment = new MessageAttachment('https://imgur.com/nc4YY3P.png');
+          const connection = await message.member.voice.channel.join();
+          //message.channel.send(attachment);
+          const dispatcher = connection.play('./record/cares.mp3');
+        } else {
+          message.reply('a coglione entra in vocale per farlo');
+        }
+      }
     }
   
   
