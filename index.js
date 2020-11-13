0c46fc91dd1ea637e221e6d623623f035e64dc9d
TeaCup2 <47120050+TeaCup2@users.noreply.github.com>
2020-11-13 11:32:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -475,6 +475,29 @@ client.on('message', async message => {
           message.reply('a coglione entra in vocale per farlo');
         }
       }
+             break;
+      case '/directed':{
+        if (message.member.voice.channel) {
+          //const attachment = new MessageAttachment('https://imgur.com/nc4YY3P.png');
+          const connection = await message.member.voice.channel.join();
+          //message.channel.send(attachment);
+          const dispatcher = connection.play('./record/directed.mp3');
+        } else {
+          message.reply('a coglione entra in vocale per farlo');
+        }
+      }
+        
+             break;
+      case '/sike':{
+        if (message.member.voice.channel) {
+          //const attachment = new MessageAttachment('https://imgur.com/nc4YY3P.png');
+          const connection = await message.member.voice.channel.join();
+          //message.channel.send(attachment);
+          const dispatcher = connection.play('./record/sike.mp3');
+        } else {
+          message.reply('a coglione entra in vocale per farlo');
+        }
+      }
  
     }
   
